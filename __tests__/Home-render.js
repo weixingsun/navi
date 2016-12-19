990f8683075a43c7748eceeb7ab537ec8c63ad0a
Weixing Sun <weixing.sun@gmail.com>
2016-12-19 23:12:17 +1300

    setup jest for home#1

--- __tests__/Home-render.js
+++ __tests__/Home-render.js
@@ -1,12 +1,51 @@
-import 'react-native';
-import React from 'react';
+'use strict'
+jest.mock('react-native-permissions',()=>{
+  const permissions = require.requireActual('react-native-permissions')
+  permissions.getPermissionStatus= function(name){
+    return new Promise(resolve => 'authorized')
+  }
+  return permissions
+})
+jest.mock('react-native-maps', () => {
+  const React = require('React')
+  return class MockMapView extends React.Component {
+    static Marker = props => React.createElement('Marker', props, props.children);
+    static propTypes = { children: React.PropTypes.any };
+
+    render() {
+      return React.createElement('MapView', this.props, this.props.children);
+    }
+  }
+});
+
+import 'react-native'
+import React from 'react'
 import Home from '../src/v/Home';
 import renderer from 'react-test-renderer';
 
-jest.mock('../__mock__/permission');
+describe('renders correctly', () => {
+  beforeEach(()=>{
+    //Home.componentWillMount = emptyFunc
+  });
+    it('on visible=true', () => {
+        const latlng = {
+            latitude: 52.5166667,
+            longitude: 13.4,
+        };
+        const color = '#ace';
 
-it('renders home page correctly', () => {
+        const component = (
+            <Home />
+        );
+
+        const tree = renderer.create(component).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+});
+
+/*it('renders home page correctly', () => {
   const tree = renderer.create(
     <Home />
   );
-});
+})*/
